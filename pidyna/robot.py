872d42c84c7b93859ebfd66977f3223622faaf6b
Doruk Arisoy <doruk.arisoy@gmail.com>
2020-02-05 13:17:57 -0800

    timeout stuff + cleanup

--- pidyna/robot.py
+++ pidyna/robot.py
@@ -13,35 +13,34 @@ class Robot:
     forearm_wrist_joint = 5
     wrist_hand_joint = 6
     hand_tool_joint = 7
+    minValue = 4
+    maxValue = 5
+    timeout = 0.05
 
     def __init__(self):
         self.servo_bus = Ax12_2()
         self.urdf = "./niryo_one.urdf"
         self.chain = ikpy.chain.Chain.from_urdf_file(self.urdf)
-        self.servoIDs = self.servo_bus.learnServos(minValue = 4, maxValue=5,verbose=True)
+        self.servoIDs = self.learnServos(minValue, maxValue,verbose=True)
         print('servoIDs = ',self.servoIDs)
         print('enabling Torque on all servos')
         for i in self.servoIDs:
-            self.servo_bus.setTorqueStatus(i, True,verbose=True)
-        print('done initializing') 
-
-    def moveJoint(self, joint, position, verbose=False):
-        if joint in self.servoIDs:
-            if verbose: print("moving joint ",joint," to position ", position)
-            #if verbose: print("current position ", self.servo_bus.readPositionDegrees(joint))
-            self.servo_bus.moveDegrees( joint, position)
+            # self.servo_bus.setTorqueStatus(i, True,verbose=True)
+            self.setTorque(i, True, verbose=True)
+        print('done initializing')
 
     def move3d(self,position):
         #position is 3d np vector, or 3 element list
         target_frame = np.eye(4)
         target_frame[:3, 3] = position
-        degrees = self.chain.inverse_kinematics(target_frame) * (180 / np.pi) 
+        degrees = self.chain.inverse_kinematics(target_frame) * (180 / np.pi)
         print(degrees)
         self.moveJoint(Robot.elbow_forearm_joint, degrees[Robot.elbow_forearm_joint],verbose=True)
-    
+
     def record(self):
         for i in self.servoIDs:
-            self.servo_bus.setTorqueStatus(i,False)
+            # self.servo_bus.setTorqueStatus(i,False)
+            self.setTorque(i, False)
         arr = []
         try:
             while True:
@@ -52,13 +51,67 @@ class Robot:
             print(e)
             return arr
 
+    def moveJoint(self, joint, position, verbose=False):
+        #TODO: if timout send again
+        def timeout(signum, frame):
+            raise IOError("Timeout")
+
+        try :
+            signal.signal(signal.SIGALRM, timeout)
+            signal.alarm(1)
+            if joint in self.servoIDs:
+                if verbose: print("moving joint ",joint," to position ", position)
+                self.servo_bus.moveDegrees( joint, position)
+
+        except Exception as detail:
+            if verbose : print("Error setting torque on servo #" + str(i) + ': ' + str(detail))
+            sleep(0.5)
+            moveJoint(joint, position, verbose)
+            pass
+        signal.alarm(0)
+
+    def setTorque(self, id, status, verbose=False):
+        #TODO: if timout send again
+        def timeout(signum, frame):
+            raise IOError("Timeout")
+
+        try :
+            signal.signal(signal.SIGALRM, timeout)
+            signal.alarm(1)
+            temp = self.servo_bus.setTorqueStatus(id, status ,verbose)
+            if verbose: print('set torque: ', status, ' on servo #', id)
+
+        except Exception as detail:
+            if verbose : print("Error setting torque on servo #" + str(i) + ': ' + str(detail))
+            sleep(0.5)
+            setTorque(id, status, verbose)
+            pass
+        signal.alarm(0)
+
+    def learnServos(self, minValue=1, maxValue=6, verbose=False):
+        servoList = []
+        def timeout(signum, frame):
+            raise IOError("Timeout")
+
+        for i in range(minValue, maxValue + 1):
+            try :
+                signal.signal(signal.SIGALRM, timeout)
+                signal.alarm(1)
+                temp = self.servo_bus.ping(i)
+                servoList.append(i)
+                if verbose: print("Found servo #" + str(i))
+
+            except Exception as detail:
+                if verbose : print("Error pinging servo #" + str(i) + ': ' + str(detail))
+                pass
+        signal.alarm(0)
+        return servoList
+
     def goHome(self):
         val = -30
         while 1:
             sleep(1)
             self.moveJoint(Robot.elbow_forearm_joint, val,verbose=True)
-            #sleep(1)
             self.moveJoint(Robot.forearm_wrist_joint, val,verbose=True)
-            #sleep(1)
             self.moveJoint(Robot.wrist_hand_joint, val,verbose=True)
             val += 5
