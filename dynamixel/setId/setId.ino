588a8bfb8618bd6a1d4c3069f4e390f1e6ea66e8
Brandon <jbiams77@gmail.com>
2020-02-27 06:04:42 -0800

    merging NiryoOne ROS

---
+++ dynamixel/setId/setId.ino
@@ -0,0 +1,69 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include <DynamixelShield.h>
+
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
+  #define DEBUG_SERIAL soft_serial
+#elif defined(ARDUINO_SAM_DUE) || defined(ARDUINO_SAM_ZERO)
+  #define DEBUG_SERIAL SerialUSB    
+#else
+  #define DEBUG_SERIAL Serial
+#endif
+
+const uint8_t DXL_ID = 1;
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+DynamixelShield dxl;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Use UART port of DYNAMIXEL Shield to debug.
+  DEBUG_SERIAL.begin(115200);
+  
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(1000000);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+
+    
+    
+    
+
+    int i,j;
+    for(i=0; i<10; i++){
+      // set a new ID for DYNAMIXEL. Do not use ID 200
+      dxl.setID(i, 2);
+      DEBUG_SERIAL.println("ID has been successfully changed to 100");
+      for(j=0; j<i; j++){
+          if(dxl.ping(i)){
+            dxl.ledOn(i);
+            delay(500);
+            dxl.ledOff(i);
+            delay(500);
+        }
+      }
+    }
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
