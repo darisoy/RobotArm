4b4a3dc2e69ed0ab8282865f9dafa4e057e7ef63
d1r1karsy <doruk.arisoy@gmail.com>
2020-03-03 16:47:54 -0800

    test

--- pidyna/BrandonsAnnoyingTest.py
+++ pidyna/BrandonsAnnoyingTest.py
@@ -2,8 +2,15 @@ import robot
 import time
 
 my_robot = robot.Robot()
+my_robot.goReady()
+delay = 1
+angle = 30
 
 while True:
-	time.sleep(1)
-	my_robot.goReady()
+    time.sleep(delay)
+    angle *= -1
+    my_robot.moveLR(angle)
+    my_robot.moveFB(angle)
+    my_robot.moveUD(angle)
+    
 
